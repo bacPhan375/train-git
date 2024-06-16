d32d11d13d9d9ed7e1df30e02bfa5281a827cca7
Bac <bacphan375@gmail.com>
2024-06-16 22:02:33 +0700

    fix xong roi ne

--- train.cpp
+++ train.cpp
@@ -10,9 +10,11 @@ int main(){
   int a = 0 ; 
   int b =0; 
   cout <<"nhap a:"<<endl;
+  cout << "hello moi nguoi toi moi dc them";
+  // chan qua di a lam sao day 
   cin >> a; 
   cout <<"nhap b:"<<endl;
   cin >> b;
-  cout <<"tong a va b: "<< a+b<<endl;
+  cout <<"tong a va b: "<< a*b<<endl;
   return 0;
 }
