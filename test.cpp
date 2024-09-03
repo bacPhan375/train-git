2d565a16e70193a69b0af19ae2e74c3311f25d97
Bac <bacphan375@gmail.com>
2024-09-03 20:20:49 +0700

    update

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std; 
+
+int main (){
+    cout<< "hello moi nguoi"; 
+    return 0;
+}
--- train.cpp
+++ train.cpp
@@ -7,6 +7,7 @@ int main(){
   cout <<"co su thay doi moi" ; 
   cout <<"thay doi nay hoi bi nhieu do";
   cout <<"thay doi moi"; 
+   cout <<"tong a va b: "<< a*b<<endl;
   int a = 0 ; 
   int b =0; 
   cout <<"nhap a:"<<endl;
@@ -16,5 +17,7 @@ int main(){
   cout <<"nhap b:"<<endl;
   cin >> b;
   cout <<"tong a va b: "<< a*b<<endl;
+   cout <<"tong a va b: "<< a*b<<endl;
+    cout <<"tong a va b: "<< a*b<<endl;
   return 0;
 }
