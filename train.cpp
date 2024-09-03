6b119dc66bc72734b0649950ac77dfea6e7011f3
Bac <bacphan375@gmail.com>
2024-09-03 20:33:02 +0700

    a

--- train.cpp
+++ train.cpp
@@ -5,18 +5,16 @@ int main(){
   int x = 0 ; 
   // cout <<"hello moi người "<<endl ; 
   cout <<"co su thay doi moi" ; 
-  cout <<"thay doi nay hoi bi nhieu do";
+  cout <<"thay  hoi bi nhieu do";
   cout <<"thay doi moi"; 
   int a = 0 ; 
-  int b =0; 
+  int b =1; 
   cout <<"nhap a:"<<endl;
-  cout << "hello moi nguoi toi moi dc them";
+  cout << " nguoi toi moi dc them";
   // chan qua di a lam sao day 
   cin >> a; 
   cout <<"nhap b:"<<endl;
   cin >> b;
-  cout <<"tong a va b: "<< a*b<<endl;
-   cout <<"tong a va b: "<< a*b<<endl;
-    cout <<"tong a va b: "<< a*b<<endl;
+  cout <<"tong a va b la bang bao nhieu: "<< a*b<<endl;
   return 0;
 }
